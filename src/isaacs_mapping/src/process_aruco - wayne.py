c07ff4c432bd0f7f04d51f2c2d9f94eca4c23513
Weiyan Zhu <weiyanz@berkeley.edu>
2021-04-01 20:38:06 -0700

    get camera pose, not sure if its useful though

--- src/isaacs_mapping/src/process_aruco - wayne.py
+++ src/isaacs_mapping/src/process_aruco - wayne.py
@@ -17,8 +17,10 @@ rpy_array = []
 R_array = []
 pose_is_set = False
 image_subsriber = None
+pose_subsriber = None
 publisher = None
 image_publisher = None
+camera_pose = None
 conversion_matrix = [[1, 0, 0, 0], 
 					[0, 1, 0, 0], 
 					[0, 0, 1, 0], 
@@ -54,7 +56,6 @@ def convert_point_clouds(pointcloud_data):
 	for p in reader:
 		#transfer point to aruco marker's corrdinate system
 		new_p = list(np.matmul(conversion_matrix, [p[0], p[1], p[2], 1]))
-		#print(new_p[0:3] + [list(p)[3:]])
 		new_points.append(new_p[0:3] + list(p)[3:])
 	return point_cloud2.create_cloud(pointcloud_data.header, pointcloud_data.fields, new_points)
 
@@ -68,14 +69,9 @@ def try_set_pose(image):
 
 	#print("camera_matrix: " + str(camera_matrix))
 	#print("dist_coeffs: " + str(dist_coeffs))
-	
 
-	# Capture frame-by-frame, take only left camera image
-	#ret, frame = cap.read()
 	frame = image
 
-	#frame = np.split(frame, 2, axis=1)[0]
-	#cv2.imshow('zed', frame)
 	# Our operations on the frame come here
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -84,12 +80,11 @@ def try_set_pose(image):
 
 	#lists of ids and the corners belonging to each id
 	corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, dictionary=aruco_dict, cameraMatrix=camera_matrix,distCoeff=dist_coeffs)
-	markerLength = 0.16
+	markerLength = 0.08
 
 	rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerLength, camera_matrix, dist_coeffs)
 	if(rvec is None or tvec is None):
 		return False
-	print("rvec not None here:", rvec)
 	rvec = rvec[0][0]
 	tvec = tvec[0][0]
 	# we need a homogeneous matrix but OpenCV only gives us a 3x3 rotation matrix
@@ -98,58 +93,39 @@ def try_set_pose(image):
 				[0, 0, 0, tvec[2]],
 				[0, 0, 0, 1]],
 								        dtype=float)
-	rotation_matrix[:3, :3], _ = cv2.Rodrigues(rvec)
+	rotation_matrix[:3, :3], _ = cv2.Rodrigues(rvec) #marker in camera coordinate system 
 	R = np.linalg.inv(rotation_matrix)
-	R_array.append(R)
 
 	#get average R
 	global conversion_matrix
+	#when we detect marker camera is not at (0,0,0), need to take camera's pose into account
+	global camera_pose
+	camera_R = np.array([[0, 0, 0, camera_pose.position.x],
+			[0, 0, 0, camera_pose.position.y],
+			[0, 0, 0, camera_pose.position.z],
+			[0, 0, 0, 1]],
+	 						dtype=float)
+	camera_rvec = euler_from_quaternion(camera_pose.orientation.x, camera_pose.orientation.y, 
+										camera_pose.orientation.z, camera_pose.orientation.w)
+	camera_R[:3, :3], _ = cv2.Rodrigues(camera_rvec)
+	camera_R = np.linalg.inv(camera_R)
+	R_array.append(R)
+
 	if len(R_array) >= 20: 
 		R_sum = R_array[0]
 		for i in range(1, len(R_array)):
+			print(i, R_array[i])
 			R_sum += R_array[i]
 		conversion_matrix = R_sum / len(R_array)
 		return True
 	return False
-'''
-	# convert the matrix to a quaternion
-	quaternion = tf.transformations.quaternion_from_matrix(R)
-
-	# To visualize in rviz, you can, for example, publish a PoseStamped message:
-	rpy_array.append([R[0][3], R[1][3], R[2][3], quaternion[0], quaternion[1], quaternion[2], quaternion[3]])
-
-	if len(rpy_array) >= 20:
-		last_arrays = rpy_array[-3:]
-		s0, s1, s2, s3, s4, s5, s6 = 0, 0, 0, 0, 0, 0, 0  # sums
-		for arr in last_arrays:
-			s0 += arr[0]
-			s1 += arr[1]
-			s2 += arr[2]
-			s3 += arr[3]
-			s4 += arr[4]
-			s5 += arr[5]
-			s6 += arr[5]
-
-		denom = len(last_arrays)
-		s0 /= denom
-		s1 /= denom
-		s2 /= denom
-		s3 /= denom
-		s4 /= denom
-		s5 /= denom
-		s6 /= denom
-		
- 		print('Setting camera pose with tvec: ' + str([s0, s1, s2]) + ', rvec: ' + str([s3, s4, s5, s6]))
-		#setZEDPose(s0, s1, s2, s3, s4, s5, s6)
-		#start_mapping_service = rospy.ServiceProxy('/zed2/zed_node/start_3d_mapping', start_3d_mapping)
-		#ret = start_mapping_service(0.2, 8.0, 1.0) # resolution, range (meters), PC freq
-'''
 
 def process_image(image_data):
 	image_serialized = np.fromstring(image_data.data, np.uint8)#np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
 	image_deserialized = cv2.imdecode(image_serialized, cv2.IMREAD_COLOR)
 	global pose_is_set
 	global image_subsriber
+	global pose_subsriber
 	global image_publisher
 	if(pose_is_set):
 		return
@@ -158,13 +134,19 @@ def process_image(image_data):
 		print("finish set pose")
 		pose_is_set = True
 		image_subsriber.unregister()
+		pose_subsriber.unregister()
 		rospy.Subscriber('/zed2/zed_node/mapping/fused_cloud', PointCloud2, convert_zed_pose)
 
+def update_camera_pose(data):
+	global camera_pose
+	camera_pose = data.pose
+	#print(camera_pose)
 
 def track_marker():
 	global pose_is_set
 	global publisher
 	global image_subsriber
+	global pose_subsriber
 	print("Waiting for services...")
 	#rospy.wait_for_service('/zed2/zed_node/set_pose')
 	#rospy.wait_for_service('/zed2/zed_node/start_3d_mapping')
@@ -172,15 +154,10 @@ def track_marker():
 	
 	rospy.init_node('tracking_data', anonymous=True)
 	image_subsriber = rospy.Subscriber('/zed2/zed_node/rgb/image_rect_color/compressed', CompressedImage, process_image)
+	pose_subsriber = rospy.Subscriber('/zed2/zed_node/pose', PoseStamped, update_camera_pose)
 	publisher = rospy.Publisher('converted_cloud', PointCloud2, queue_size=10)
 	image_publisher = rospy.Publisher('converted_cloud', PointCloud2, queue_size=10)
 	rospy.spin()
-	#pose_is_set = True
-	
-	#cap = cv2.VideoCapture(0, cv2.CAP_V4L)
-	
-	#while not pose_is_set:
-	#	try_set_pose(cap)
 	
 
 # source: https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
