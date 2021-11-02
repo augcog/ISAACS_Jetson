b6b4482d93c80396195a69abb8dd218d9dc9c2e4
Weiyan Zhu <weiyanz@berkeley.edu>
2021-11-02 15:18:18 -0700

    Update process_fisheye.py

--- src/isaacs_mapping/src/process_fisheye.py
+++ src/isaacs_mapping/src/process_fisheye.py
@@ -3,10 +3,12 @@ import rospy
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image
 import cv2
-import numpy as np
+import time
 
 SETTINGS = {
 	"fisheye_image_topic" : "fisheye_rgb",
+	"fps" : 60,
+	"camera_index" : 0
 }
 
 # read rgb images from a fisheye camera and publish it as a ros topic
@@ -15,12 +17,33 @@ class FisheyeProcessor:
 		rospy.init_node('fisheye_processor', anonymous=True)
 		self.image_publisher = rospy.Publisher(SETTINGS["fisheye_image_topic"], Image, queue_size = 20)
 		rospy.spin()
+		self.process_camera(SETTINGS["fps"], SETTINGS["camera_index"])
+
+	def process_camera(self, fps, cameraIndex):
+		vid = cv2.VideoCapture(cameraIndex)
+		prevTime = time.time()
+		timer = 0
+		timeInterval = 1.0/fps
+		while(True):
+			currentTime = time.time()
+			timer += (currentTime - prevTime)
+			prevTime = currentTime 
+			# Capture the video frame by frame
+			if(timer >= timeInterval):
+				timer -= timeInterval
+				ret, frame = vid.read()
+				self.image_publisher.publish(frame)
+
+			# the 'q' button is set as the quitting button
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+		
+		# After the loop release the cap object
+		vid.release()
 
 
 	def publish_image(self, im):
 		self.image_publisher.publish(im)
-
-
 
 if __name__ == '__main__':
 	print('Node to publish fisheye info start....')
