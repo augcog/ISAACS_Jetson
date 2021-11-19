fe62f90ca50f3a6e85177c456a7f02adfa650073
Weiyan Zhu <weiyanz@berkeley.edu>
2021-11-18 21:01:36 -0800

    get position and rotation for zed cam

--- src/isaacs_mapping/src/camera_processor.py
+++ src/isaacs_mapping/src/camera_processor.py
@@ -3,6 +3,8 @@ import rospy
 import pyzed.defines as sl
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Quaternion
 import cv2
 import time
 import FisheyeProcessor from "fisheye_processor"
@@ -24,11 +26,15 @@ ZED_SETTINGS = {
     "camera1_left_image_topic" : "zed1_left_rgb",
     "camera1_right_image_topic" : "zed1_right_rgb",
     "camera1_left_depth_topic" : "zed1_left_depth",
+    "camera1_position_topic" : "zed1_position",
+    "camera1_rotation_topic" : "zed1_rotation",
 
     "camera2_index" : 1,
     "camera2_left_image_topic" : "zed2_left_rgb",
     "camera2_right_image_topic" : "zed2_right_rgb",
-    "camera2_left_depth_topic" : "zed2_left_depth"
+    "camera2_left_depth_topic" : "zed2_left_depth",
+    "camera2_position_topic" : "zed2_position",
+    "camera2_rotation_topic" : "zed2_rotation"
 }
 
 # read infomation from cameras
@@ -37,13 +43,7 @@ class CameraProcessor:
     #set up cameras and ros publisher
 	def start(self):		
 		rospy.init_node('camera_processer', anonymous=True)
-        self.fisheye_image_publisher = rospy.Publisher(FISHEYE_SETTINGS["image_topic"], Image, queue_size = 20)
-        self.zed1_left_image_publisher = rospy.Publisher(ZED_SETTINGS["camera1_left_image_topic"], Image, queue_size = 20)
-        self.zed1_right_image_publisher = rospy.Publisher(ZED_SETTINGS["camera1_right_image_topic"], Image, queue_size = 20)
-        self.zed1_left_depth_publisher = rospy.Publisher(ZED_SETTINGS["camera1_left_depth_topic"], Image, queue_size = 20)
-        self.zed2_left_image_publisher = rospy.Publisher(ZED_SETTINGS["camera2_left_image_topic"], Image, queue_size = 20)
-        self.zed2_right_image_publisher = rospy.Publisher(ZED_SETTINGS["camera2_right_image_topic"], Image, queue_size = 20)
-        self.zed2_left_depth_publisher = rospy.Publisher(ZED_SETTINGS["camera2_left_depth_topic"], Image, queue_size = 20)
+        self.initialize_publisher()
 
 		rospy.spin()
 
@@ -54,6 +54,20 @@ class CameraProcessor:
         self.zed1_processor.initialize(ZED_SETTINGS["camera1_index"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
         self.zed2_processor.initialize(ZED_SETTINGS["camera2_index"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
 		self.process_camera(SETTINGS["fps"])
+
+    def initialize_publisher(self):
+        self.fisheye_image_publisher = rospy.Publisher(FISHEYE_SETTINGS["image_topic"], Image, queue_size = 20)
+        self.zed1_left_image_publisher = rospy.Publisher(ZED_SETTINGS["camera1_left_image_topic"], Image, queue_size = 20)
+        self.zed1_right_image_publisher = rospy.Publisher(ZED_SETTINGS["camera1_right_image_topic"], Image, queue_size = 20)
+        self.zed1_left_depth_publisher = rospy.Publisher(ZED_SETTINGS["camera1_left_depth_topic"], Image, queue_size = 20)
+        self.zed1_position_publisher = rospy.Publisher(ZED_SETTINGS["camera1_position_topic"], Vector3, queue_size = 20)
+        self.zed1_rotation_publisher = rospy.Publisher(ZED_SETTINGS["camera1_rotation_topic"], Quaternion, queue_size = 20)
+
+        self.zed2_left_image_publisher = rospy.Publisher(ZED_SETTINGS["camera2_left_image_topic"], Image, queue_size = 20)
+        self.zed2_right_image_publisher = rospy.Publisher(ZED_SETTINGS["camera2_right_image_topic"], Image, queue_size = 20)
+        self.zed2_left_depth_publisher = rospy.Publisher(ZED_SETTINGS["camera2_left_depth_topic"], Image, queue_size = 20)
+        self.zed2_position_publisher = rospy.Publisher(ZED_SETTINGS["camera2_position_topic"], Vector3, queue_size = 20)
+        self.zed2_rotation_publisher = rospy.Publisher(ZED_SETTINGS["camera2_rotation_topic"], Quaternion, queue_size = 20)
 
     #publishing images from cameras at each frame to a ros topic
 	def process_camera(self, fps):
@@ -68,10 +82,10 @@ class CameraProcessor:
 			# Capture the video frame by frame
 			if(timer >= timeInterval):
 				timer -= timeInterval
-				fisheye_ret, fisheye_frame = self.fisheye_processor.get_info()
-                zed1_left, zed1_right, zed1_depth = self.zed1_processor.get_info()
-                zed2_left, zed2_right, zed2_depth = self.zed2_processor.get_info()
-				self.image_publisher.publish(frame, zed1_left, zed1_right, zed1_depth, zed2_left, zed2_right, zed2_depth)
+				fisheye_ret, self.fisheyeIm = self.fisheye_processor.get_info()
+                self.zed1_left, self.zed1_right, self.zed1_depth, self.zed1_pos, self.zed1_rotation = self.zed1_processor.get_info()
+                self.zed2_left, self.zed2_right, self.zed2_depth, self.zed2_pos, self.zed2_rotation = self.zed2_processor.get_info()
+				self.image_publisher.publish()
 
 			# the 'q' button is set as the quitting button
 			if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -81,14 +95,19 @@ class CameraProcessor:
 		vid.release()
 
     #publish ros topics of the images info
-	def publish_image(self, fisheyeIm, zed1_left, zed1_right, zed1_depth, zed2_left, zed2_right, zed2_depth):
-		self.fisheye_image_publisher.publish(fisheyeIm)
-        self.zed1_left_image_publisher.publish(zed1_left)
-        self.zed1_right_image_publisher.publish(zed1_right)
-        self.zed1_left_depth_publisher.publish(zed1_depth)
-        self.zed2_left_image_publisher.publish(zed2_left)
-        self.zed2_right_image_publisher.publish(zed2_right)
-        self.zed2_left_depth_publisher.publish(zed2_depth)
+	def publish_image(self):
+		self.fisheye_image_publisher.publish(self.fisheyeIm)
+        self.zed1_left_image_publisher.publish(self.zed1_left)
+        self.zed1_right_image_publisher.publish(self.zed1_right)
+        self.zed1_left_depth_publisher.publish(self.zed1_depth)
+        self.zed1_position_publisher.publish(self.zed1_pos)
+        self.zed1_rotation_publisher.publish(self.zed1_rotation)
+
+        self.zed2_left_image_publisher.publish(self.zed2_left)
+        self.zed2_right_image_publisher.publish(self.zed2_right)
+        self.zed2_left_depth_publisher.publish(self.zed2_depth)
+        self.zed2_position_publisher.publish(self.zed2_pos)
+        self.zed2_rotation_publisher.publish(self.zed2_rotation)
 
 if __name__ == '__main__':
 	print('Node to publish cameras info start....')
--- src/isaacs_mapping/src/zed_processor.py
+++ src/isaacs_mapping/src/zed_processor.py
@@ -13,6 +13,7 @@ class ZedProcessor:
         init.camera_linux_id = cameraIndex
         init.camera_fps = fps
         self.cam = zcam.PyZEDCamera()
+
         if not cam.is_opened():
             print("Opening ZED Camera " + string(cameraIndex) + "...")
         status = cam.open(init)
@@ -20,17 +21,34 @@ class ZedProcessor:
             print(repr(status))
             exit()
         self.runtime = zcam.PyRuntimeParameters()
+
+        tracking_parameters = sl.PositionalTrackingParameters()
+        err = zed.enable_positional_tracking(tracking_parameters)
 	
 	def get_info(self) -> '[left, right, left_depth]':
         leftMat = core.PyMat()
         rightMat = core.PyMat()
         depthMat = core.PyMat()
+        zed_pose = sl.Pose()
         err = self.cam.grab(self.runtime)
         if err == tp.PyERROR_CODE.PySUCCESS:
             self.cam.retrieve_image(leftMat, sl.VIEW.LEFT)
             self.cam.retrieve_image(rightMat, sl.VIEW.RIGHT)
             self.cam.retrieve_image(depthMat, sl.VIEW.DEPTH)
-        return [leftMat.get_data(), rightMat.get_data(), depthMat.get_data()]
+            # Get the pose of the camera relative to the world frame
+            state = self.cam.get_position(zed_pose, sl.REFERENCE_FRAME.FRAME_WORLD)
+            # translation
+            py_translation = sl.Translation()
+            tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
+            ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
+            tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
+            # orientation quaternion
+            py_orientation = sl.Orientation()
+            ox = round(zed_pose.get_orientation(py_orientation).get()[0], 3)
+            oy = round(zed_pose.get_orientation(py_orientation).get()[1], 3)
+            oz = round(zed_pose.get_orientation(py_orientation).get()[2], 3)
+            ow = round(zed_pose.get_orientation(py_orientation).get()[3], 3)
+        return [leftMat.get_data(), rightMat.get_data(), depthMat.get_data(), py_translation, py_orientation]
 
 	def close(self, im):
         self.cam.close()
