21a0bb26125909434c16e9b7591a4cd51649a4da
Nitzan Orr <weiyanzhu0724@gmail.com>
2021-11-19 16:24:43 -0800

    fix indentation for camera processor

--- src/isaacs_mapping/src/camera_processor.py
+++ src/isaacs_mapping/src/camera_processor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import rospy
-import pyzed.defines as sl
+import pyzed.sl as sl
 import cv2
 import time
-import FisheyeProcessor from "fisheye_processor"
-import ZedProcessor from "zed_processor"
+from fisheye_processor import FisheyeProcessor 
+#from zed_processor import ZedProcessor 
 
 
 SETTINGS = {
@@ -13,12 +13,12 @@ SETTINGS = {
 
 FISHEYE_SETTINGS = {
     "camera1_topic_prefix" : "fisheye1",
-	"camera1_index" : 0
+    "camera1_index" : 0
 }
 
 ZED_SETTINGS = {
     "resolution" : sl.PyRESOLUTION.PyRESOLUTION_HD720,
-	"camera1_index" : 0,
+    "camera1_index" : 0,
     "camera1_topic_prefix" : "zed1",
     "camera2_index" : 1,
     "camera2_topic_prefix" : "zed2",
@@ -32,16 +32,16 @@ class CameraProcessor:
 		rospy.init_node('camera_processer', anonymous=True)
 		rospy.spin()
 
-        self.fisheye_processor = FisheyeProcessor()
-        self.fisheye_processor.initialize(FISHEYE_SETTINGS["camera1_index"], FISHEYE_SETTINGS["camera1_topic_prefix"])
+		self.fisheye_processor = FisheyeProcessor()
+		self.fisheye_processor.initialize(FISHEYE_SETTINGS["camera1_index"], FISHEYE_SETTINGS["camera1_topic_prefix"])
 
-        self.zed1_processor = ZedProcessor()
-        self.zed1_processor.initialize(ZED_SETTINGS["camera1_index"], ZED_SETTINGS["camera1_topic_prefix"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
-       
-        self.zed2_processor = ZedProcessor()
-        self.zed2_processor.initialize(ZED_SETTINGS["camera2_index"], ZED_SETTINGS["camera2_topic_prefix"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
-		
-        self.process_camera(SETTINGS["fps"])
+		#self.zed1_processor = ZedProcessor()
+		#self.zed1_processor.initialize(ZED_SETTINGS["camera1_index"], ZED_SETTINGS["camera1_topic_prefix"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
+	       
+		#self.zed2_processor = ZedProcessor()
+		#self.zed2_processor.initialize(ZED_SETTINGS["camera2_index"], ZED_SETTINGS["camera2_topic_prefix"], ZED_SETTINGS["resolution"], SETTINGS["fps"])
+			
+		self.process_camera(SETTINGS["fps"])
 
     #publishing images from cameras at each frame to a ros topic
 	def process_camera(self, fps):
@@ -56,8 +56,8 @@ class CameraProcessor:
 			if(timer >= timeInterval):
 				timer -= timeInterval
 				self.fisheye_processor.grab()
-                self.zed1_processor.grab()
-                self.zed2_processor.grab()
+		        #self.zed1_processor.grab()
+		        #self.zed2_processor.grab()
 
 			# the 'q' button is set as the quitting button
 			if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -65,8 +65,8 @@ class CameraProcessor:
 		
 		# After the loop release the cap object
 		self.fisheye_processor.close()
-        self.zed1_processor.close()
-        self.zed2_processor.close()
+		#self.zed1_processor.close()
+		#self.zed2_processor.close()
 
 if __name__ == '__main__':
 	print('Node to publish cameras info start....')
